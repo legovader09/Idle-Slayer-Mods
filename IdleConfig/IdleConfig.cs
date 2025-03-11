13f26fe9a73b362ae37879701d8da0d170f89a06
Dominik <1706584+legovader09@users.noreply.github.com>
2025-03-11 13:58:25 +0000

    Add ability to load textures into mod via core library

---
+++ IdleConfig/IdleConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using BepInEx;
+using IdleSlayerMods.Common;
+using TMPro;
+using UnityEngine;
+
+namespace IdleConfig;
+
+public class IdleConfig : MonoBehaviour
+{
+    private void Start()
+    {
+        Plugin.Log.LogInfo(Path.Combine(Paths.PluginPath, "IdleConfig", "IdleConfigIcon.png"));
+        var texture = ModHelper.LoadTextureFromFile(Path.Combine(Paths.PluginPath, "IdleConfig", "IdleConfigIcon.png"));
+
+        Plugin.ModHelper.AddPanelButton("Idle Config", ClickAction, texture);
+    }
+
+    private static void ClickAction()
+    {
+        Plugin.ModHelper.ShowDialog("Idle Config");
+    }
+}
---
+++ IdleConfig/Plugin.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using IdleSlayerMods.Common;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+namespace IdleConfig;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BasePlugin
+{
+    internal static new ManualLogSource Log;
+    internal static ModHelper ModHelper;
+
+    public override void Load()
+    {
+        Log = base.Log;
+
+        ModHelper.ModHelperMounted += helper => ModHelper = helper; 
+        SceneManager.sceneLoaded += (UnityAction<Scene, LoadSceneMode>)OnSceneLoaded;
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name != "Game") return;
+        AddComponent<IdleConfig>();
+        SceneManager.sceneLoaded -= (UnityAction<Scene, LoadSceneMode>)OnSceneLoaded;
+    }
+}
