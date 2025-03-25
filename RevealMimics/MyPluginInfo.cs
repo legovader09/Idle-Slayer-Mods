b04e42fce0cdb793a82d3b2901c248aa83be08d4
Dominik <1706584+legovader09@users.noreply.github.com>
2025-03-25 13:32:46 +0000

    Updated plugin info

--- RevealMimics/MyPluginInfo.cs
+++ RevealMimics/MyPluginInfo.cs
@@ -4,6 +4,6 @@ public static class MyPluginInfo
 {
     public const string PLUGIN_GUID = "RevealMimics";
     public const string PLUGIN_NAME = "Chest Hunt Revealer";
-    public const string PLUGIN_VERSION = "1.2.0";
+    public const string PLUGIN_VERSION = "1.2.1";
     public const string PLUGIN_AUTHOR = "Doomnik";
 }
