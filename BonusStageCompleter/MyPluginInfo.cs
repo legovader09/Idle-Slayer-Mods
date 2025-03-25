aea853d98fb7922af8eb9e7997b74f27b4ffd108
Dominik <1706584+legovader09@users.noreply.github.com>
2025-03-25 21:01:00 +0000

    Update Mod Template README

--- BonusStageCompleter/MyPluginInfo.cs
+++ BonusStageCompleter/MyPluginInfo.cs
@@ -4,6 +4,6 @@ public static class MyPluginInfo
 {
     public const string PLUGIN_GUID = "BonusStageCompleter";
     public const string PLUGIN_NAME = "Bonus Stage Completer";
-    public const string PLUGIN_VERSION = "1.0.1";
+    public const string PLUGIN_VERSION = "1.0.2";
     public const string PLUGIN_AUTHOR = "Doomnik";
 }
