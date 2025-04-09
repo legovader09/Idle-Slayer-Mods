f404a3d92c4f5f6e7f37a5d3feb09f98415fd780
Dominik <1706584+legovader09@users.noreply.github.com>
2025-04-09 21:58:22 +0100

    Added debug log extension for easier debugging

---
+++ IdleSlayerMods.Common/Extensions/LoggerExtension.cs
@@ -0,0 +1,13 @@
+﻿using MelonLoader;
+
+namespace IdleSlayerMods.Common.Extensions;
+
+public static class LoggerExtension
+{
+    public static void Debug(this MelonLogger.Instance logger, string message)
+    {
+        #if DEBUG
+        logger.MsgPastel(ConsoleColor.DarkGray, message);
+        #endif
+    }
+}
