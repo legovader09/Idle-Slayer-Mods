db4d4410f449161f2a7cf82c95cac2e0dd923593
Dominik <1706584+legovader09@users.noreply.github.com>
2025-04-09 22:11:10 +0100

    remove if debug region

--- IdleSlayerMods.Common/Extensions/LoggerExtension.cs
+++ IdleSlayerMods.Common/Extensions/LoggerExtension.cs
@@ -6,8 +6,6 @@ public static class LoggerExtension
 {
     public static void Debug(this MelonLogger.Instance logger, string message)
     {
-        #if DEBUG
         logger.MsgPastel(ConsoleColor.DarkGray, message);
-        #endif
     }
 }
