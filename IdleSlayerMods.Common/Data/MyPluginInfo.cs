5bd1e389da521f5b72a422fa0cb0944c7d6bbf59
Dominik <1706584+legovader09@users.noreply.github.com>
2026-03-26 19:20:06 +0000

    Update Plugin Version

--- IdleSlayerMods.Common/Data/MyPluginInfo.cs
+++ IdleSlayerMods.Common/Data/MyPluginInfo.cs
@@ -5,7 +5,7 @@ public static class MyPluginInfo
 {
     public const string PLUGIN_GUID = "IdleSlayerMods.Common";
     public const string PLUGIN_NAME = "Idle Slayer Mods Core";
-    public const string PLUGIN_VERSION = "1.3.0";
+    public const string PLUGIN_VERSION = "1.3.1";
     public const string PLUGIN_AUTHOR = "Doomnik";
     public const string GAME_DEVELOPER = "Pablo Leban";
     public const string GAME_NAME = "Idle Slayer";
